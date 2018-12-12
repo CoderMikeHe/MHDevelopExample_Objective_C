338f2db02a7608c25e106d3f6de87676292f5c81
CoderMikeHe <491273090@qq.com>
2018-12-12 23:02:37 +0800

    新增MHObject -- CoderMikeHe

---
+++ MHDevelopExample/MHDevelopExample/Classes/BaseClass/Model/MHObject.h
@@ -0,0 +1,17 @@
+//
+//  MHObject.h
+//  MHDevelopExample
+//
+//  Created by CoderMikeHe on 2018/12/12.
+//  Copyright © 2018 CoderMikeHe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MHObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MHDevelopExample/MHDevelopExample/Classes/BaseClass/Model/MHObject.m
@@ -0,0 +1,13 @@
+//
+//  MHObject.m
+//  MHDevelopExample
+//
+//  Created by CoderMikeHe on 2018/12/12.
+//  Copyright © 2018 CoderMikeHe. All rights reserved.
+//
+
+#import "MHObject.h"
+
+@implementation MHObject
+
+@end
